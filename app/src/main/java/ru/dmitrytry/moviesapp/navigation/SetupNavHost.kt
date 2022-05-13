094f7ba5a916e8930c77f0201b15850e6715edb6
Dmitry <dmitry_eagles@mail.ru>
2022-05-13 16:36:38 +0300

    video start screen

---
+++ app/src/main/java/ru/dmitrytry/moviesapp/navigation/SetupNavHost.kt
@@ -0,0 +1,33 @@
+package ru.dmitrytry.moviesapp.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import ru.dmitrytry.moviesapp.screens.MainScreen
+import ru.dmitrytry.moviesapp.screens.SplashScreen
+import ru.dmitrytry.moviesapp.until.Constants
+
+sealed class Screens(val route: String){
+    object Splash: Screens(route = Constants.Screens.SPLASH_SCREENS)
+    object Main: Screens(route = Constants.Screens.MAIN_SCREENS)
+    object Details: Screens(route = Constants.Screens.DETAILS_SCREENS)
+}
+
+@Composable
+fun SetupNavHost(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = Screens.Splash.route
+    ) {
+        composable(route = Screens.Splash.route) {
+            SplashScreen(navController=navController)
+        }
+        composable(route = Screens.Main.route) {
+            MainScreen()
+        }
+        composable(route = Screens.Details.route) {
+
+        }
+    }
+}
---
+++ app/src/main/java/ru/dmitrytry/moviesapp/until/Constants.kt
@@ -0,0 +1,10 @@
+package ru.dmitrytry.moviesapp.until
+
+class Constants {
+
+    object Screens {
+        const val SPLASH_SCREENS = "splash_screen"
+        const val MAIN_SCREENS = "main_screen"
+        const val DETAILS_SCREENS = "details_screen"
+    }
+}
